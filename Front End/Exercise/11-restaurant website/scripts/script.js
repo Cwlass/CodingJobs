8746d92a1a25107a40756f9de9facb2ac6754e91
Cwlass <39944635+Cwlass@users.noreply.github.com>
2021-02-23 09:00:03 +0100

    Restaurant

---
+++ Front End/Exercise/11-restaurant website/scripts/script.js
@@ -0,0 +1,27 @@
+function checkTime() {
+    const now = new Date();
+    let currentHour = now.getHours(); // -> 10
+    let currentMinutes = now.getMinu
+
+    //Open or Close
+    if (((currentHour > 10) && (currentHour < 14) || ((currentHour > 19) && (currentHour < 23)))) {
+        document.querySelector('#blackBg').innerHTML = "Open"
+    } else {
+        document.querySelector('#blackBg').innerHTML = "Closed"
+    }
+    //Closing or Opening soon
+    if ((currentMinutes >= 30) && (currentHour == 14 || currentHour == 17 || currentHour == 10 || currentHour == 22)) {
+        if (currentHour == 14 && currentMinutes >= 30 || currentHour == 17 && currentMinutes >= 30) {
+            document.querySelector('#whiteBg').innerHTML = 'Closes in ' + (60 - currentMinutes);
+        }
+        if (currentHour == 10 && currentMinutes >= 30 || currentHour == 22 && currentMinutes >= 30) {
+            document.querySelector('#whiteBg').innerHTML = 'Opens in ' + (60 - currentMinutes);
+        }
+    } else {
+        document.querySelector('#whiteBg').style.display = "none"
+    }
+}
+//Initial check
+checkTime();
+//Check again every minute
+var t = setInterval(checkTime(), 60000);
