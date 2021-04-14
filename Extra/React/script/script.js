f866eea5225dfae65bbac364808a3f4f63c19d6b
Cwlass <39944635+Cwlass@users.noreply.github.com>
2021-04-14 17:03:05 +0200

    Reworked

--- Extra/React/script/script.js
+++ Extra/React/script/script.js
@@ -16,18 +16,48 @@ class PlayerGenerator extends React.Component {
             charPortrait: "",
             charFC: "",
         };
+        this.handleChange = this.handleChange.bind(this);
     }
-    async getPlayer() {
-        const charURL = 'https://xivapi.com/character/search?name=' + this.state.charFName + '+' + this.state.charSName + '&' + this.state.selectedServer;
+    handleChange(event) {
+        this.setState({ selectedServer: event.target.value })
+    }
+    async fcClick(fcmember) {
+        const newName = fcmember.replace(" ", "+");
+        console.log(newName);
+        const charURL = 'https://xivapi.com/character/search?name=' + newName + '&server=' + this.state.selectedServer;
+        //console.log(charURL)
         const res = await fetch(charURL);
         const result = await res.json();
+        console.log(result);
         this.setState({ charID: result.Results[0].ID });
         const char = await fetch('https://xivapi.com/character/' + this.state.charID);
         const charResult = await char.json();
         this.setState({ charName: charResult.Character.Name })
         this.setState({ charNameDay: charResult.Character.Nameday })
         this.setState({ charFC: charResult.Character.FreeCompanyName })
+        this.setState({ FcID: charResult.Character.FreeCompanyId })
+
         this.setState({ charPortrait: charResult.Character.Portrait })
+        document.querySelector("#charImg").classList.remove("charImg");
+    }
+    async getPlayer() {
+        document.querySelector("#charImg").classList.add("charImg");
+        const charURL = 'https://xivapi.com/character/search?name=' + this.state.charFName + '+' + this.state.charSName + '&server=' + this.state.selectedServer;
+        //console.log(charURL)
+        const res = await fetch(charURL);
+        const result = await res.json();
+        console.log(result);
+        this.setState({ charID: result.Results[0].ID });
+        const char = await fetch('https://xivapi.com/character/' + this.state.charID);
+        const charResult = await char.json();
+        this.setState({ charName: charResult.Character.Name })
+        this.setState({ charNameDay: charResult.Character.Nameday })
+        this.setState({ charFC: charResult.Character.FreeCompanyName })
+        this.setState({ FcID: charResult.Character.FreeCompanyId })
+
+        this.setState({ charPortrait: charResult.Character.Portrait })
+        document.querySelector("#charImg").classList.remove("charImg");
+        this.getFc();
     }
     changeFName = (e) => {
         this.setState({ charFName: e.target.value });
@@ -38,7 +68,15 @@ class PlayerGenerator extends React.Component {
     selectServer = (e) => {
         this.setState({ selectedServer: e.target.value });
     }
-
+    async getFc() {
+        const fcURL = 'https://xivapi.com/freecompany/' + this.state.FcID + '?data=FCM';
+        console.log(fcURL);
+        const res = await fetch(fcURL);
+        const result = await res.json();
+        console.log(result.FreeCompanyMembers);
+        this.setState({ FcMembers: result.FreeCompanyMembers });
+        console.log(this.state.FcMembers);
+    }
 
     async componentDidMount() {
         const res = await fetch('https://xivapi.com/servers');
@@ -63,7 +101,7 @@ class PlayerGenerator extends React.Component {
                             <label for="character">Character Name</label>
                         </div>
                         <div>
-                            <select name="Server" class="categories" value={this.state.selectServer}>
+                            <select name="Server" class="categories" onChange={this.handleChange} value={this.state.selectedServer}>
                                 {this.state.serverList.map((server) => (
                                     <option value={server}>{server}</option>
                                 ))}
@@ -74,6 +112,7 @@ class PlayerGenerator extends React.Component {
                     </article>
                     <img src="ff14Logo.png" alt="" />
                 </section>
+
                 <div id="container">
                     <article id="player">
                         <div>
@@ -81,11 +120,12 @@ class PlayerGenerator extends React.Component {
                             <h3 id="level">{this.state.charNameDay}</h3>
                             <h4 id="FC">{this.state.charFC}</h4>
                             <ul class="fcMembers">
-                                <li>
-                                </li>
+                                {this.state.FcMembers.map((fcmember) => (
+                                    <li onClick={() => this.fcClick(fcmember.Name)} > {fcmember.Name}</li>
+                                ))}
                             </ul>
                         </div>
-                        <img src={this.state.charPortrait} alt="" />
+                        <img class="charImg" id="charImg" src={this.state.charPortrait} alt="" />
                     </article>
                 </div>
             </div >
