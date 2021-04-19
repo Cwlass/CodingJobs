a607451a08bbc31993933fe77447651f53c5e578
Cwlass <39944635+Cwlass@users.noreply.github.com>
2021-04-19 10:17:24 +0200

    Cleaned up PI exercise

--- Pi/script.js
+++ Pi/script.js
@@ -1,6 +1,7 @@
 let pointsTotal = 0;
 let pointsInCircle = 0;
 
+
 function calculatePI() {
     let x = Math.random();
     let y = Math.random();
@@ -10,16 +11,9 @@ function calculatePI() {
     pointsTotal++
     document.querySelector('p').innerText = 4 * (pointsInCircle / pointsTotal);
 
-    return 4 * (pointsInCircle / pointsTotal)
 }
 setInterval(() => {
     calculatePI()
 }, 1000 / 60)
 
-function btnPressed() {
-    document.querySelector('p').innerText = calculatePI(document.querySelector('input').value);
-}
 
-document.querySelector('button').addEventListener('click', btnPressed);
-
-console.log(calculatePI(document.querySelector('input').innerText));
